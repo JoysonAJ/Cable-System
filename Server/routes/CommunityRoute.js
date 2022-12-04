6bfca684fcd103b029d75aa2cbf3f6ec13758274
JoysonAJ <joysondsouzaaj@gmail.com>
2022-12-04 12:47:30 +0530

    community  backend done

--- Server/routes/CommunityRoute.js
+++ Server/routes/CommunityRoute.js
@@ -21,5 +21,48 @@ router.post("/question", async (req, res) => {
     }
 })
 
+router.get("/getquestion", async (req, res) => {
+
+    try {
+        const getQuestion = await communityModel.find({})
+        // console.log(getQuestion)
+        res.status(200).send(getQuestion)
+    } catch (e) {
+        res.status(400).json({message: e})
+    }
+
+
+})
+
+router.post("/response", async (req, res) => {
+    try {
+        let responseArray = new Array();
+        const data = req.body
+        console.log(data)
+        const updateRespose = {
+            name: req.body.name,
+            userType: req.body.userType,
+            responseQuestion: req.body.response
+        }
+        const findId = req.body.upadteId
+        const reqQuestionObj = await communityModel.findOne({_id: findId})
+        console.log(reqQuestionObj)
+        const existedResponse = reqQuestionObj.response
+        existedResponse &&
+        existedResponse.map(responseData => {
+            responseArray.push(responseData)
+        })
+        responseArray.push(updateRespose)
+        reqQuestionObj.response = responseArray
+
+        await reqQuestionObj.save();
+
+        res.status(200).send("here we go")
+    } catch (e) {
+        res.status(404).json({message: e.stack})
+    }
+
+})
+
 
 module.exports = router;
